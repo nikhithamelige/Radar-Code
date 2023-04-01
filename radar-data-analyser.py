273d336106ad61ca82fbf32ec7e7f8281c443eaa
Debjyoti Chowdhury <consentium.inc@gmail.com>
2023-04-01 21:34:15 +0530

    room data analyser

---
+++ radar-data-analyser.py
@@ -0,0 +1,65 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# range_data_path = "data/empty_room_t.csv" #empty
+
+range_data_path = "data/occupied_room_t.csv"  # occupied
+
+df = pd.read_csv(range_data_path, index_col=False)
+
+# Config parameters for test
+configParameters = {'numDopplerBins': 16, 'numRangeBins': 128, 'rangeResolutionMeters': 0.04360212053571429,
+                    'rangeIdxToMeters': 0.04360212053571429, 'dopplerResolutionMps': 0.12518841691334906,
+                    'maxRange': 10.045928571428572, 'maxVelocity': 2.003014670613585}
+
+# Generate the range and doppler arrays for the plot
+rangeArray = np.array(range(configParameters["numRangeBins"])) * configParameters["rangeIdxToMeters"]
+dopplerArray = np.multiply(np.arange(-configParameters["numDopplerBins"] / 2, configParameters["numDopplerBins"] / 2),
+                           configParameters["dopplerResolutionMps"])
+
+
+def calculate_range_doppler_heatmap(payload, config):
+    # Convert levels to dBm
+    payload = 10 * np.log10(payload)
+    # Clac. range Doppler array
+    rangeDoppler = np.reshape(payload, (config["numDopplerBins"], config["numRangeBins"]), 'F')
+    rangeDoppler = np.append(rangeDoppler[int(len(rangeDoppler) / 2):], rangeDoppler[:int(len(rangeDoppler) / 2)],
+                             axis=0)
+    return rangeDoppler
+
+
+def apply_2d_cfar(signal, guard_band_width, kernel_size, threshold_factor):
+    num_rows, num_cols = signal.shape
+    thresholded_signal = np.zeros((num_rows, num_cols))
+    for i in range(guard_band_width, num_rows - guard_band_width):
+        for j in range(guard_band_width, num_cols - guard_band_width):
+            # Estimate the noise level
+            noise_level = np.mean(np.concatenate((
+                signal[i - guard_band_width:i + guard_band_width, j - guard_band_width:j + guard_band_width].ravel(),
+                signal[i - kernel_size:i + kernel_size, j - kernel_size:j + kernel_size].ravel())))
+            # Calculate the threshold for detection
+            threshold = threshold_factor * noise_level
+            # Check if the signal exceeds the threshold
+            if signal[i, j] > threshold:
+                thresholded_signal[i, j] = 1
+    return thresholded_signal
+
+
+guard_band_width = 3
+kernel_size = 3
+threshold_factor = 1
+
+fig = plt.figure()
+
+for i in df.columns:
+    doppler_data = df[i].to_numpy()
+    range_doppler = calculate_range_doppler_heatmap(doppler_data, configParameters)
+    detected_objects = apply_2d_cfar(range_doppler, guard_band_width, kernel_size, threshold_factor)
+
+    plt.clf()
+    cs = plt.contourf(rangeArray, dopplerArray, range_doppler)
+    fig.colorbar(cs, shrink=0.9)
+    fig.canvas.draw()
+    plt.pause(0.1)
+
